fbcb5c86ff524e2d08030049b53efeb09dbd4e85
Jonathan Livingstone <j_livingstone@outlook.com>
2019-04-30 17:37:46 -0400

    cleaned up javascript

--- InfinityPlay/Scripts/Site.js
+++ InfinityPlay/Scripts/Site.js
@@ -35,22 +35,3 @@ window.addEventListener('load', function () {
 
 // ----------------------- Media Player ----------------------------------
 
-window.onload = function () {
-
-    var myAudio = document.getElementById('my-audio');
-    var play = document.getElementById('play');
-    var pause = document.getElementById('pause');
-
-    // associate functions with the 'onclick' events
-    play.onclick = playAudio;
-    pause.onclick = pauseAudio;
-
-    function playAudio() {
-        myAudio.play();
-    }
-
-    function pauseAudio() {
-        myAudio.pause();
-    }
-
-}
